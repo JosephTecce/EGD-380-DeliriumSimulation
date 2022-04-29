00e828a16f4c9c344c0abf308e42d9fa89273a28
wesley Elmer <wesley.elmer@mymail.champlain.edu>
2022-04-28 23:34:12 -0400

    Polishing Model Viewer just a little

--- EGD-380-DeliriumSimulation/Assets/_DeliruimSimulation/Scripts/RotateObject.cs
+++ EGD-380-DeliriumSimulation/Assets/_DeliruimSimulation/Scripts/RotateObject.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class RotateObject : MonoBehaviour
 {
     private Vector3 _rotation;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 10f;
 
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A)) _rotation = Vector3.up;
-        else if (Input.GetKey(KeyCode.D)) _rotation = Vector3.down;
-        else _rotation = Vector3.zero;
+        // reset rotation value of object
+        _rotation = Vector3.zero;
 
+        // do the stupidest thing I've ever written. 
+        // i won't even comment how this works because you shouldn't do it this way,
+        // you should completely re-write it from scratch using Unity's input axis system and Quaternions and also use the mouse to rotate the model
+        
+        if ( Input.GetKey( KeyCode.A ) || Input.GetKey( KeyCode.LeftArrow) )        _rotation = Vector3.up;
+        else if ( Input.GetKey( KeyCode.D ) || Input.GetKey( KeyCode.RightArrow ) ) _rotation = Vector3.down;
+        else if ( Input.GetKey( KeyCode.W ) || Input.GetKey( KeyCode.UpArrow) )     _rotation = Vector3.right;
+        else if ( Input.GetKey( KeyCode.S ) || Input.GetKey( KeyCode.DownArrow) )   _rotation = Vector3.left;
 
         transform.Rotate(_rotation * _speed * Time.deltaTime);
     }
